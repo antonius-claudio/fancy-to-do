5d20b219a28f7eb7feffe7c6903db0d0079bfaca
Claudio Antonius <antonius.claudio@gmail.com>
2020-03-30 14:42:03 +0700

    get, update

--- server/controllers/controllerTodos.js
+++ server/controllers/controllerTodos.js
@@ -2,7 +2,7 @@ const { Todo } = require('../models');
 
 class controllerTodos {
     static create(req, res){
-        const { title, description, status, due_date } = req.body;
+        let { title, description, status, due_date } = req.body;
         Todo.create({
             title,
             description,
@@ -10,22 +10,65 @@ class controllerTodos {
             due_date
         })
             .then((newTodo) => {
-                res.status(201).json({newTodo});
+                res.status(201).json({ newTodo });
             }).catch((err) => {
                 if (err.errors) {
-                    res.status(400).json({errors: err.errors});
-                }                
-                res.status(500).json(err);
+                    res.status(400).json({ errors: err.errors });
+                } else {
+                    res.status(500).json(err);
+                }          
             });
     }
 
     static getTodos(req, res){
         Todo.findAll()
             .then((todos) => {
-                res.status(200).json({todos});
+                res.status(200).json({ todos });
             }).catch((err) => {
                 res.status(500).json(err);
             });
+    }
+
+    static getById(req, res){
+        let id = req.params.id;
+        Todo.findByPk(id)
+            .then((todo) => {
+                if (todo === null) {
+                    res.status(404).json({ errors: "Not Found!" })
+                } else {
+                    res.status(200).json({ todo });
+                }
+            }).catch((err) => {
+                res.status(500).json(err);
+            });
+    }
+
+    static update(req, res){
+        let { title, description, status, due_date } = req.body;
+        let id = req.params.id;
+        Todo.update({
+            title,
+            description,
+            status,
+            due_date
+        }, { where: { id } })
+            .then((updateTodo) => {
+                if (updateTodo[0] === 1) {
+                    res.status(200).json({ updateTodo: { title, description, status, due_date } });
+                } else {
+                    res.status(404).json({ errors: "ID is not registered!"});
+                }
+            }).catch((err) => {
+                if (err.errors) {
+                    res.status(400).json({ errors: err.errors });
+                } else {
+                    res.status(500).json(err);
+                }
+            });
+    }
+
+    static delete(req, res){
+
     }
 }
 
--- server/routes/routerTodos.js
+++ server/routes/routerTodos.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 const controller = require('../controllers/controllerTodos');
 
 router.post('/', controller.create);
+
 router.get('/', controller.getTodos);
+
+router.get('/:id', controller.getById);
+
+router.put('/:id', controller.update);
+
+router.delete('/:id', controller.delete);
 
 module.exports = router;
