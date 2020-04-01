f5ff7b79a4f04265a49007b5787d975a24790069
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-01 15:30:03 +0700

    client

---
+++ client/assets/main.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+    let isLogged = false;
+    if (isLogged) {
+        $('#menuRegis').hide();
+        $('#menuLogin').hide();
+        $('#menuTodo').show();
+        $('#menuLogout').show();
+        $('#divLogin').hide();
+        $('#divRegister').hide();
+        $('#divTodo').show();
+
+    } else {
+        $('#menuTodo').hide();
+        $('#menuLogout').hide();
+    }
+    $('#divRegister').hide();
+    $('#divLogin').hide();
+    $('#divTodo').hide();
+
+    $('#menuRegis').click(function() {
+        $('#divLogin').hide();
+        $('#divTodo').hide();
+        $('#menuLogout').hide();
+        $('#divRegister').show();
+    })
+
+    $('#menuLogin').click(function() {
+        $('#divTodo').hide();
+        $('#divRegister').hide();
+        $('#menuLogout').hide();
+        $('#divLogin').show();
+    })
+
+    $('#formRegister').submit(function (e) {
+        e.preventDefault();
+        let email = $('emailRegister').val();
+        let password = $('passwordRegister').val();
+        // M.toast({html: 'Registered'});
+        
+        // AJAX POST
+        $.ajax({
+            url: "http://localhost:3000/register",
+            method: "POST",
+            data: {
+                email, 
+                password
+            }
+        })
+        .done((result) => {
+            $('.test').append(`<p>berhasil: ${JSON.stringify(result)}</p>`);
+        })
+        .fail((err) => {
+            M.toast({html: `Error : ${JSON.stringify(err)}`});
+        })
+    });
+})
